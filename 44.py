a2f40115e2ed7d10cf3ecf728a24e12623303141
Vasil Bandarenka <VBandarenka@gomel.iba.by>
2020-04-15 19:07:40 +0300

    first

---
+++ 44.py
@@ -0,0 +1,8 @@
+from sys import argv
+script, first, second, third = argv
+print("Этот сценарий называется:", script)
+print("Моя вторая переменная называется:", first)
+print("Моя вторая переменная:", second)
+print("Моя третья переменная:", third)
+input(f"{first}")
+x = input('?')
